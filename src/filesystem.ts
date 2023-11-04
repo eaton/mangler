a1354771f2e7893ea15b73de884bce0885bb1db7
Jeff Eaton <jeff@eaton.fyi>
2023-11-04 15:56:10 -0500

    Adding fs-jetpack to the mix

---
+++ src/filesystem.ts
@@ -0,0 +1,13 @@
+import jetpack from "fs-jetpack";
+
+/**
+ * An extremely lightweight wrapper for the fs-jetpack library.
+ * 
+ * May be adding some extras to it over time — particularly, configurable file format
+ * helpers that would allow `read()` and `write()` to handle things other than JSON
+ * automatically. JSONC, NDJSON, JSON5, CSV, TSV, and YAML are the prime candidates
+ * for convenience wrappers.
+ * 
+ * For now, though, jetpack's the way to go.
+ */
+export const fs = jetpack;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-export * from './markup/index.js';
 export * as Text from './text/index.js';
 export * as Ids from './ids/index.js';
 export * as Dates from './dates/index.js';
 
+
+export * from './filesystem.js';
+export * from './markup/index.js';
 export * from './misfits/php.js';
