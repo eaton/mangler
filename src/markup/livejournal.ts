bfbfb419d853d7b681229ed58797fee746853985
Jeff Eaton <jeff@eaton.fyi>
2023-11-05 16:02:16 -0600

    Taking a stab at an alternate mechanism for LJ-Cut processing. Won't spend much more time on it, but stubbing it out feels useful.

--- src/markup/livejournal.ts
+++ src/markup/livejournal.ts
@@ -1,20 +1,42 @@
 /**
  * Livejournal and its forked derivatives (most notably dreamwidth)
  * supported a handful of custom tags.
+ * 
+ * In particular, a number of variations on the lj-cut tag are supported;
+ * while this interperetation of tag behavior doesn't make a lot of sense
+ * according to the HTML spec, it is an attempt to faithfully reproduce
+ * the way the `<lj-cut>` tag actually behaved on the service. Such is life.
+ * 
+ * pre <lj-cut> hidden
+ * pre <lj-cut text="text"> hidden
+ * 
+ * pre <lj-cut /> hidden
+ * pre <lj-cut text="text" /> hidden
+ * 
+ * pre <lj-cut></lj-cut> hidden
+ * pre <lj-cut text="text"></lj-cut> hidden
+ * 
+ * pre <lj-cut>hidden</lj-cut> post
+ * pre <lj-cut text="text">hidden</lj-cut> post
  */
 
-const patterns = {
+
+export const patterns = {
   user: /<lj user=['"]?(\w*)['"]?[^>]*>/ig,
   cutWrapper: /<lj-cut\s+text=['"]?([^'">]*)['"]?>(.+)<\/lj-cut>/ig, // <lj-cut text="cut text">Hidden content</lj-cut>
-  cutWrapperNoText: /<lj-cut[^>]*>(.+)<\/lj-cut>/ig,                       // <lj-cut>Hidden content</lj-cut>
+  cutWrapperNoText: /<lj-cut[^>]*>(.+)<\/lj-cut>/ig,                 // <lj-cut>Hidden content</lj-cut>
   cutClosed: /<lj-cut\s+text=['"]?([^'">]*)['"]?[^>]*><\/lj-cut>/ig, // <lj-cut text="cut text"></lj-cut>
-  cutClosedNoText: /<lj-cut[^>]*><\/lj-cut>/ig,                              // <lj-cut></lj-cut>
-  cutSelfClosing: /<lj-cut\s+text=['"]?([^'">]*)['"]?[^>]*>/ig,       // <lj-cut text="cut text">
-  cutSelfClosingNoText: /<lj-cut[^>]*>/ig,                                   // <lj-cut>
+  cutClosedNoText: /<lj-cut[^>]*><\/lj-cut>/ig,                      // <lj-cut></lj-cut>
+  cutSelfClosing: /<lj-cut\s+text=['"]?([^'">]*)['"]?[^>]*>/ig,      // <lj-cut text="cut text">
+  cutSelfClosingNoText: /<lj-cut[^>]*>/ig,                           // <lj-cut>
 }
 
 /**
  * Generate an entry teaser from an entry with lj-cut tags.
+ * 
+ * There are a couple of permutations that make handling cut tags in an
+ * output-agnostic manner difficult; 
+ * 
  */
 export function cutTeaser(html: string) {
   const wrapperReplacement = '<span class="lj-cut">$1</span>';
@@ -22,13 +44,44 @@ export function cutTeaser(html: string) {
   const placeholder = '\ufeff';
 
   return html
-    .replace(patterns.cutWrapper, wrapperReplacement)                          // Replace wrapper with text
-    .replace(patterns.cutWrapperNoText, wrapperNoTextReplacement)                          // Replace wrapper with text
-    .replace(patterns.cutSelfClosing, wrapperReplacement + placeholder)                // Replace breaker with placeholder
-    .replace(patterns.cutClosedNoText, placeholder) // Replace annotated breaker with text & placeholder
+    .replace(patterns.cutWrapper, wrapperReplacement)                   // Replace wrapper with text
+    .replace(patterns.cutWrapperNoText, wrapperNoTextReplacement)       // Replace wrapper with text
+    .replace(patterns.cutSelfClosing, wrapperReplacement + placeholder) // Replace breaker with placeholder
+    .replace(patterns.cutClosedNoText, placeholder)                     // Replace annotated breaker with text & placeholder
     .replace(patterns.cutClosed, wrapperReplacement + placeholder)      // Replace annotated breaker with text & placeholder
     .replace(patterns.cutSelfClosingNoText, placeholder)                // Replace breaker with placeholder
     .split(placeholder)[0];                                             // Discard post-placeholder text
+}
+
+
+type CutResults = {
+  /**
+   * Event markup appearing before the lj-cut tag. If no cut tag is used,
+   * only this value will be populated.
+   */
+  pre?: string,
+
+  /**
+   * The custom cut text entered by the event's author.
+   */
+  text?: string,
+  
+  /**
+   * The event content hidden by the `lj-cut` tag. In most cases, this will be
+   * the remainder of the event but there may be additional un-hidden text after
+   * the cut.
+   */
+  hidden?: string,
+  
+  /**
+   * Any event markup appearing after the `<lj-cut>` tag is closed.
+   */
+  post?: string,
+};
+
+export function processCutTag(html: string) {
+  const output: CutResults = {};
+  return output;
 }
 
 /**
