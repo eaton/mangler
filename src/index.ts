8ba7b83b12ec6e8db23dcd49896a52d33d344c35
Jeff Eaton <jeff@eaton.fyi>
2023-11-13 18:06:56 -0600

    Mobing jetpack to Files to live alongside image and video manip

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * as Ids from './ids/index.js';
 export * as Dates from './dates/index.js';
 export * as Urls from './urls/index.js';
 
-export * from './filesystem.js';
+export * from './files/index.js';
 export * from './markup/index.js';
 export * from './formats/index.js';
 
