fe8a55dd3cbab9448ff4bc41f9e0bae5276e6365
Jeff Eaton <jeff@eaton.fyi>
2023-11-19 02:30:48 -0600

    Markdown default tweats

--- src/markup/markdown.ts
+++ src/markup/markdown.ts
@@ -3,13 +3,20 @@ import { toText as htmlToText } from './html/index.js';
 import TurndownService from 'turndown';
 
 export function toHtml(input: string) {
-  return parse(input);
+  return parse(input, { gfm: true });
 }
 
 export function toText(input: string) {
   return htmlToText(parse(input));
 }
-export function fromHtml(input: string) {
-  const turndownService = new TurndownService();
+export function fromHtml(input: string, options: TurndownService.Options = {}) {
+  const turndownService = new TurndownService({
+    hr: '---',
+    emDelimiter: '*',
+    headingStyle: 'atx',
+    bulletListMarker: '-',
+    codeBlockStyle: 'fenced',
+    ...options
+  });
   return turndownService.turndown(input);
 }
