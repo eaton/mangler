5985a22441962346bbca976a6eb3b307220d4953
Jeff Eaton <jeff@eaton.fyi>
2023-11-20 09:25:30 -0600

    Package notes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+/**
+ * A collection of utility libraries for data parsing, manipulation, and conversion
+ * with somewhat-opiionated consistency wrappers. 
+ * 
+ * @remarks
+ * Mangler is meant to speed the process of large-scale migration and import projects,
+ * where working with many different types of incoming data can be frustrating to to
+ * the inconsistencies between the tools needed to deal with each one.
+ * 
+ * If you're interested in just one or two of its helper functions, you should absolutely
+ * check out the underlying libraries it wraps: they'll be a much more efficient way to
+ * get exactly what you need. 
+ * 
+ * @packageDocumentation
+ */
+
 export * as Text from './text/index.js';
 export * as Ids from './ids/index.js';
 export * as Dates from './dates/index.js';
