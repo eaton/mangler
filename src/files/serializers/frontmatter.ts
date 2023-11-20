c282d1ab658850d4a1d34a06e29094ac97646435
Jeff Eaton <jeff@eaton.fyi>
2023-11-20 09:25:44 -0600

    Separate type for tidiness.

--- src/files/serializers/frontmatter.ts
+++ src/files/serializers/frontmatter.ts
@@ -3,13 +3,15 @@ import { Serializer } from 'fs-jetpack/types';
 import jetpack from 'fs-jetpack';
 import { isObject } from '@sindresorhus/is';
 
+type FrontmatterInput = { content: string, data: Record<string, unknown> };
+
 export const Frontmatter: Serializer<
-  { content: string, data: Record<string, unknown> },
+FrontmatterInput,
   GrayMatterFile<string>
 > = {
   validate: (data: unknown) => isObject(data),
   parse: (input: string) => matter(input),
-  stringify: (input: { content: string, data: Record<string, unknown> }) => {
+  stringify: (input: FrontmatterInput) => {
     const { content, data } = input;
     return matter.stringify(content, data);
   },
