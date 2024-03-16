dcc8791a1352c5f54956d82607bf918bdfb5feba
Jeff Eaton <jeff@eaton.fyi>
2024-03-16 11:35:09 -0500

    Fix for frontmatter encoding/decoding

--- src/files/serializers/frontmatter.ts
+++ src/files/serializers/frontmatter.ts
@@ -1,9 +1,10 @@
 import matter, { GrayMatterFile } from 'gray-matter';
+import { encode, decode } from 'entities';
 import { Serializer } from 'fs-jetpack/types';
 import jetpack from 'fs-jetpack';
 import { isObject } from '@sindresorhus/is';
 
-type FrontmatterInput = { content: string, data: Record<string, unknown> };
+export type FrontmatterInput = { content: string, data: Record<string, unknown> };
 
 export const Frontmatter: Serializer<
 FrontmatterInput,
@@ -13,7 +14,12 @@ FrontmatterInput,
   parse: (input: string) => matter(input),
   stringify: (input: FrontmatterInput) => {
     const { content, data } = input;
-    return matter.stringify(content, data);
+
+    // Some exotic characters ... caused problems. We're UTF encoding and then decoding
+    // by default, which — distressingly — seemed to solve the problems.
+    const scrubbed = decode(encode(content, { level: 1, mode: 0 }), { level: 1, mode: 0 })
+
+    return matter.stringify(scrubbed, data);
   },
 };
 
